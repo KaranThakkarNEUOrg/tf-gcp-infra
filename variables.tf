6964e249526f65ed1acb78adae107b6dfb057824
KaranThakkarNEU <thakkar.kara@northeastern.edu>
2024-02-15 02:51:44 -0500

    yml change

---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "project_id" {
+  description = "Project ID where resources will be created"
+}
+
+
+variable "region" {
+  description = "The region where resources will be created"
+}
+
+variable "credentials_file_path" {
+  description = "Path to the credentials file"
+}
